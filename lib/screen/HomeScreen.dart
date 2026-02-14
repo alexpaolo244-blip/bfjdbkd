bad0c5da0edd44580608758614bf4a7b68bc3c24
alexpaolo244-blip <alexpaolo244@gmail.com>
2026-02-14 17:59:23 +0100

    Update HomeScreen.dart

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:isolate';
-// تم حذف سطر استيراد Facebook Ads من هنا
+// تم حذف سطر استيراد Facebook Ads نهائياً
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
@@ -63,9 +63,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
   void initState() {
     WidgetsBinding.instance.addObserver(this);
     super.initState();
-    // تم حذف سطر FacebookAudienceNetwork.init لضمان عدم وجود إعلانات فيسبوك
-    Iterable mTabs = jsonDecode(getStringAsync(TABS));
-    mTabList = mTabs.map((model) => TabsResponse.fromJson(model)).toList();
+    // تم حذف سطر FacebookAudienceNetwork.init تماماً
     _getInstanceId();
     if (getStringAsync(IS_WEBRTC) == "true") {
       checkWebRTCPermission();
@@ -81,7 +79,6 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
       },
     );
     init();
-    // تم حذف استدعاء تحميل الإعلانات البينية
   }
 
   Future<bool> checkPermission() async {
@@ -105,46 +102,18 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
 
   @override
   void dispose() {
-    init();
     WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
 
   Future<void> init() async {
-    String? referralCode = getReferralCodeFromNative();
-    if (referralCode!.isNotEmpty) {
-      mInitialUrl = referralCode;
-    }
-
-    if (getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_BOTTOM_NAVIGATION_SIDE_DRAWER) {
-      Iterable mBottom = jsonDecode(getStringAsync(MENU_STYLE));
-      mBottomMenuList = mBottom.map((model) => MenuStyleModel.fromJson(model)).toList();
-    } else {
-      Iterable mBottom = jsonDecode(getStringAsync(BOTTOMMENU));
-      mBottomMenuList = mBottom.map((model) => MenuStyleModel.fromJson(model)).toList();
-    }
-    if (getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_BOTTOM_NAVIGATION || getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_BOTTOM_NAVIGATION_SIDE_DRAWER) {
-      if (mBottomMenuList.isNotEmpty) {
-        mInitialUrl = widget.mUrl;
-      } else {
-        mInitialUrl = getStringAsync(URL);
-      }
-    } else if (getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_TAB_BAR || getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_SIDE_DRAWER_TABS) {
-      log(widget.mUrl);
-      if (mTabList.isNotEmpty) {
-        mInitialUrl = widget.mUrl;
-        log(mInitialUrl);
-      } else {
-        mInitialUrl = getStringAsync(URL);
-      }
-    } else {
-      mInitialUrl = getStringAsync(URL);
-    }
+    // إجبار التطبيق على فتح موقعك الشخصي مباشرة وتجاهل أي روابط خارجية
+    mInitialUrl = "https://zarship.com/"; 
 
     if (webViewController != null) {
       await webViewController!.loadUrl(urlRequest: URLRequest(url: WebUri(mInitialUrl!)));
     } else {
-      log("sorry");
+      log("Controller not initialized yet");
     }
   }
 
@@ -173,8 +142,6 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
         webViewController!.goBack();
         return false;
       } else {
-        log("--------------Show_exit");
-        // تم حذف عرض الإعلانات البينية عند الخروج
         if (getStringAsync(IS_Exit_POP_UP) == "true") {
           return mConfirmationDialog(() {
             Navigator.of(context).pop(false);
@@ -192,7 +159,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
               future: Future.delayed(Duration(milliseconds: 200)),
               builder: (context, snapshot) {
                 return InAppWebView(
-                    initialUrlRequest: URLRequest(url: WebUri(mURL.isEmptyOrNull ? mInitialUrl.validate() : mURL!)),
+                    initialUrlRequest: URLRequest(url: WebUri(mURL.isEmptyOrNull ? mInitialUrl! : mURL!)),
                     initialSettings: InAppWebViewSettings(
                       mixedContentMode: MixedContentMode.MIXED_CONTENT_ALWAYS_ALLOW,
                       allowBackgroundAudioPlaying: true,
@@ -204,7 +171,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
                       allowFileAccessFromFileURLs: true,
                       useOnDownloadStart: true,
                       javaScriptCanOpenWindowsAutomatically: true,
-                      javaScriptEnabled: getStringAsync(IS_JAVASCRIPT_ENABLE) == "true" ? true : false,
+                      javaScriptEnabled: true,
                       supportZoom: getStringAsync(IS_ZOOM_FUNCTIONALITY) == "true" ? true : false,
                       incognito: getStringAsync(IS_COOKIE) == "true" ? true : false,
                       clearCache: getStringAsync(IS_COOKIE) == "true" ? true : false,
@@ -216,7 +183,6 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
                       webViewController = controller;
                     },
                     onLoadStart: (controller, url) {
-                      log("onLoadStart");
                       if (getStringAsync(IS_LOADER) == "true") appStore.setLoading(true);
                       setState(() {});
                     },
@@ -228,93 +194,17 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
                       }
                     },
                     onLoadStop: (controller, url) async {
-                      log("onLoadStop");
                       pullToRefreshController!.endRefreshing();
                       if (getStringAsync(IS_LOADER) == "true") appStore.setLoading(false);
-
-                      await webViewController!.evaluateJavascript(source: """
-                        if (typeof XMLHttpRequest.prototype.originalOpen === 'undefined') {
-                          XMLHttpRequest.prototype.originalOpen = XMLHttpRequest.prototype.open;
-                          XMLHttpRequest.prototype.open = function(method, url) {
-                            this.originalOpen.apply(this, arguments);
-                            if (url.includes('.mp3') || url.includes('.m4a')) {
-                              this.setRequestHeader('Range', 'bytes=0-');
-                            }
-                          };
-                        }
-                      """);
-                      if (getStringAsync(DISABLE_HEADER) == "true") {
-                        webViewController!
-                            .evaluateJavascript(source: "javascript:(function() { " + "var head = document.getElementsByTagName('header')[0];" + "head.parentNode.removeChild(head);" + "})()")
-                            .then((value) => debugPrint('Page finished loading Javascript'))
-                            .catchError((onError) => debugPrint('$onError'));
-                      }
-
-                      if (getStringAsync(DISABLE_FOOTER) == "true") {
-                        webViewController!.evaluateJavascript(
-                            source: "javascript:(function() {"
-                                + "var footer = document.getElementsByTagName('footer')[0];"
-                                + "if (footer) footer.parentNode.removeChild(footer);"
-                                + "var customFooter = document.querySelector('section[data-ppt-blockid=\"footer1\"]');"
-                                + "if (customFooter) customFooter.parentNode.removeChild(customFooter);"
-                                + "console.log('Footer removed');"
-                                + "})()"
-                        ).then((value) => debugPrint('Footer removal script executed'))
-                            .catchError((onError) => debugPrint('$onError'));
-                      }
-                      await webViewController!.evaluateJavascript(
-                          source: """
-                            console.log("WebView is ready.");
-                            document.querySelectorAll('audio, video').forEach(media => {
-                              media.setAttribute('preload', 'auto');
-                              if(media.tagName === 'VIDEO') {
-                                media.setAttribute('playsinline', 'true');
-                                media.setAttribute('webkit-playsinline', 'true');
-                              }
-                            });
-                          """
-                      );
-                      setState(() {});
-                    },
-                    onReceivedError: (InAppWebViewController controller, WebResourceRequest request, WebResourceError error) {
-                      log("onLoadError");
-                      if (getStringAsync(IS_LOADER) == "true") appStore.setLoading(false);
-                      pullToRefreshController!.endRefreshing();
                       setState(() {});
                     },
                     shouldOverrideUrlLoading: (controller, navigationAction) async {
-                      var uri = navigationAction.request.url;
                       var url = navigationAction.request.url.toString();
-                      
-                      if (url.startsWith('tg:') || url.contains('t.me') || url.contains('telegram.me')) {
-                        try {
-                          final username = _extractTelegramUsername(url);
-                          if (username != null) {
-                            final appUri = Uri.parse('tg://resolve?domain=$username');
-                            if (await canLaunchUrl(appUri)) {
-                              await launchUrl(appUri, mode: LaunchMode.externalNonBrowserApplication);
-                            } else {
-                              await launchUrl(Uri.parse('https://t.me/$username'), mode: LaunchMode.externalApplication);
-                            }
-                          }
-                          return NavigationActionPolicy.CANCEL;
-                        } catch (e) {
-                          return NavigationActionPolicy.CANCEL;
-                        }
-                      }
-
-                      if (url.contains("linkedin.com") || url.contains("whatsapp://") || url.contains("tel:") || url.contains("mailto:")) {
-                        try {
-                          await launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication);
-                          return NavigationActionPolicy.CANCEL;
-                        } catch (e) {
-                          return NavigationActionPolicy.CANCEL;
-                        }
+                      if (url.contains("whatsapp://") || url.contains("tel:") || url.contains("mailto:") || url.contains("t.me")) {
+                        await launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication);
+                        return NavigationActionPolicy.CANCEL;
                       }
                       return NavigationActionPolicy.ALLOW;
-                    },
-                    onDownloadStartRequest: (controller, downloadStartRequest) {
-                      launchUrl(Uri.parse(downloadStartRequest.url.toString()), mode: LaunchMode.externalApplication);
                     }).visible(isWasConnectionLoss == false);
               }),
           Container(color: Colors.white, height: context.height(), width: context.width(), child: Loaders(name: appStore.loaderValues).center()).visible(appStore.isLoading)
@@ -327,45 +217,21 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
         child: SafeArea(
           child: Scaffold(
             drawerEdgeDragWidth: 0,
-            appBar: getStringAsync(NAVIGATIONSTYLE) != NAVIGATION_STYLE_FULL_SCREEN
-                ? PreferredSize(
-                    child: AppBarComponent(
-                      onTap: (value) {
-                        if (value == RIGHT_ICON_RELOAD) webViewController!.reload();
-                        if (RIGHT_ICON_SHARE == value) Share.share(getStringAsync(SHARE_CONTENT));
-                        if (LEFT_ICON_HOME == value) DashBoardScreen().launch(context);
-                        if (LEFT_ICON_BACK_1 == value || LEFT_ICON_BACK_2 == value) _exitApp();
-                      },
-                    ),
-                    preferredSize: Size.fromHeight((getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_TAB_BAR ||
-                            getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_SIDE_DRAWER ||
-                            getStringAsync(HEADERSTYLE) == HEADER_STYLE_CENTER ||
-                            getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_BOTTOM_NAVIGATION_SIDE_DRAWER ||
-                            getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_SIDE_DRAWER_TABS)
-                        ? 60.0 : 0.0),
-                  )
-                : PreferredSize(child: SizedBox(), preferredSize: Size.fromHeight(0.0)),
+            appBar: PreferredSize(
+              child: AppBarComponent(
+                onTap: (value) {
+                  if (value == RIGHT_ICON_RELOAD) webViewController!.reload();
+                  if (RIGHT_ICON_SHARE == value) Share.share(getStringAsync(SHARE_CONTENT));
+                  if (LEFT_ICON_HOME == value) DashBoardScreen().launch(context);
+                  if (LEFT_ICON_BACK_1 == value || LEFT_ICON_BACK_2 == value) _exitApp();
+                },
+              ),
+              preferredSize: Size.fromHeight(60.0),
+            ),
             floatingActionButton: getStringAsync(IS_FLOATING) == "true" ? FloatingComponent() : SizedBox(),
-            drawer: Drawer(
-              child: SideMenuComponent(onTap: () {
-                mInitialUrl = getStringAsync(URL).isNotEmpty ? getStringAsync(URL) : "https://www.google.com";
-                webViewController!.reload();
-              }),
-            ).visible(getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_SIDE_DRAWER ||
-                getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_BOTTOM_NAVIGATION_SIDE_DRAWER ||
-                getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_SIDE_DRAWER_TABS),
-            body: getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_TAB_BAR || getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_SIDE_DRAWER_TABS && appStore.mTabList.length != 0
-                ? TabBarView(
-                    physics: NeverScrollableScrollPhysics(),
-                    children: [
-                      for (int i = 0; i < mTabList.length; i++) mLoadWeb(mURL: mTabList[i].url),
-                    ],
-                  )
-                : mLoadWeb(mURL: mInitialUrl),
-            // تم تغيير showBannerAds() إلى SizedBox() لإلغاء مساحة الإعلان
-            bottomNavigationBar: getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_BOTTOM_NAVIGATION || getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_BOTTOM_NAVIGATION_SIDE_DRAWER
-                ? SizedBox.shrink()
-                : SizedBox(), 
+            body: mLoadWeb(mURL: mInitialUrl),
+            // تم إلغاء مساحة الإعلان نهائياً هنا
+            bottomNavigationBar: SizedBox.shrink(), 
           ),
         ),
       );
@@ -373,12 +239,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
 
     return WillPopScope(
       onWillPop: _exitApp,
-      child: getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_TAB_BAR || getStringAsync(NAVIGATIONSTYLE) == NAVIGATION_STYLE_SIDE_DRAWER_TABS
-          ? DefaultTabController(
-              length: appStore.mTabList.length,
-              child: mBody(),
-            )
-          : mBody(),
+      child: mBody(),
     );
   }
 }
